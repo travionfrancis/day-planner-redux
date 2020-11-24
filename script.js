f5feec8edc9bb31c9e69093b74e3ff7d33fed564
TraviFravi <travi1500@gmail.com>
2020-11-24 15:09:41 -0500

    Meant to work on this repo, but the LocalStorage function is now working

--- script.js
+++ script.js
@@ -6,14 +6,35 @@
 
 // Fix LocalStorage Issue: read docs and watch videos
 // just some exapmle code, not being used currently
-var someVarName = "value";
-localStorage.setItem("someVarKey", someVarName);
+$(document).ready(function () {
+    // save button actually will save on click
+    $(".saveBtn").on("click", function () {
+        console.log('Im working');
+        var time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
+        
+        localStorage.setItem(time, text);
+    });
+
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
-// display current time in Javascript
-var date = new Date();
-var n = date.toDateString();
-var time = date.toLocaleTimeString();
 
-// applying the date and time to HTML
-document.getElementById("rightNow").innerHTML = n + ' ' + time;
+
+    // display current time in Javascript
+    var date = new Date();
+    var n = date.toDateString();
+    var time = date.toLocaleTimeString();
+
+    // applying the date and time to HTML
+    document.getElementById("rightNow").innerHTML = n + ' ' + time;
+
+});
